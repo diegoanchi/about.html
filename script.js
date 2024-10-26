6d5007172f7dd6b396fb400abba76525870769a7
diegoanchi <anchi_2006@hotmail.com>
2024-10-26 00:49:24 -0500

    pc3

---
+++ script.js
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="index.html">Home</a>
+        </li>
+        <li>
+          <a href="productos.html">Productos</a>
+        </li>
+        <li>
+          <a href="contacto.html">Contact</a>
+        </li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ script_card.js
@@ -0,0 +1,28 @@
+const Card = ({ category, srcimagen, title, description, price }) => {
+  return (
+    <div class="pieRow" data-category={category}>
+      <img src={srcimagen} width="100" height="63" class="pie-img" />
+      <div>
+        <h5 class="pie-title">{title}</h5>
+        <p class="pie-description">{description}</p>
+      </div>
+      <small class="pie-price">{price}</small>
+    </div>
+  );
+};
+
+const CardList = ({ cardsData }) => (
+  <div class="content-section">
+    {cardsData.map((card, index) => (
+      <Card
+        key={index}
+        category={card.category}
+        srcimagen={card.src}
+        title={card.title}
+        description={card.description}
+        price={card.price}
+      />
+    ))}
+        
+  </div>
+);
